94ce995d7169dd10059f532300b7cc12f8a16209
hkdark github <hkngokhong@gmail.com>
2019-11-20 00:21:33 +0700

    fixed untracked files

---
+++ Plugin-ClassHtmlAnimate.py
@@ -0,0 +1,53 @@
+import sublime_plugin
+import sublime
+
+animate_classes = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello", "bounceIn", "bounceInDown", "bounceInLeft", "bounceInRight", "bounceInUp", "bounceOut", "bounceOutDown", "bounceOutLeft", "bounceOutRight", "bounceOutUp", "fadeIn", "fadeInDown", "fadeInDownBig", "fadeInLeft", "fadeInLeftBig", "fadeInRight", "fadeInRightBig", "fadeInUp", "fadeInUpBig", "fadeOut", "fadeOutDown", "fadeOutDownBig", "fadeOutLeft", "fadeOutLeftBig", "fadeOutRight", "fadeOutRightBig", "fadeOutUp", "fadeOutUpBig", "flip", "flipInX", "flipInY", "flipOutX", "flipOutY", "lightSpeedIn", "lightSpeedOut", "rotateIn", "rotateInDownLeft", "rotateInDownRight", "rotateInUpLeft", "rotateInUpRight", "rotateOut", "rotateOutDownLeft", "rotateOutDownRight", "rotateOutUpLeft", "rotateOutUpRight", "slideInUp", "slideInDown", "slideInLeft", "slideInRight", "slideOutUp", "slideOutDown", "slideOutLeft", "slideOutRight", "zoomIn", "zoomInDown", "zoomInLeft", "zoomInRight", "zoomInUp", "zoomOut", "zoomOutDown", "zoomOutLeft", "zoomOutRight", "zoomOutUp", "hinge", "jackInTheBox", "rollIn", "rollOut"]
+
+
+
+class BootstrapCompletions(sublime_plugin.EventListener):
+    """
+    Provide tag completions for Bootstrap elements and data-uk attributes
+    """
+    def __init__(self):
+
+        self.class_completions = [("%s \tAnimate Class" % s, s) for s in animate_classes]
+
+    def on_query_completions(self, view, prefix, locations):
+
+        if view.match_selector(locations[0], "text.html string.quoted"):
+
+            # Cursor is inside a quoted attribute
+            # Now check if we are inside the class attribute
+
+            # max search size
+            LIMIT  = 250
+
+            # place search cursor one word back
+            cursor = locations[0] - len(prefix) - 1
+
+            # dont start with negative value
+            start  = max(0, cursor - LIMIT - len(prefix))
+
+            # get part of buffer
+            line   = view.substr(sublime.Region(start, cursor))
+
+            # split attributes
+            parts  = line.split('=')
+
+            # is the last typed attribute a class attribute?
+            if len(parts) > 1 and parts[-2].strip().endswith("class"):
+                return self.class_completions
+            if len(parts) > 1 and parts[-2].strip().endswith("className"):
+                return self.class_completions
+            else:
+                return []
+        elif view.match_selector(locations[0], "text.html meta.tag - text.html punctuation.definition.tag.begin"):
+
+            # Cursor is in a tag, but not inside an attribute, i.e. <div {here}>
+            # This one is easy, just return completions for the data-uk-* attributes
+            return self.data_completions
+
+        else:
+
+            return []
