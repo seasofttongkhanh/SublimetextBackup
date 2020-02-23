fc5102347b5f354b0188cda68342c80c0b739b48
hkdark github <hkngokhong@gmail.com>
2020-02-23 14:14:56 +0700

    test

--- ShowSearchWindowCommand.py
+++
@@ -1,8 +0,0 @@
-import sublime
-import sublime_plugin
-
-class ShowSearchWindowCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        sublime.active_window().run_command('show_panel', args={'panel': 'output.find_results'})
-        a = sublime.active_window().find_output_panel("find_results")
-        sublime.active_window().focus_view(a)
