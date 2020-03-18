2809d43509f2829b25075d837197974a97156193
khanh github <hkngokhong@gmail.com>
2020-03-18 23:02:50 +0700

    Wed 03/18/2020

--- User/path.bat
+++ User/path.bat
@@ -1,6 +1,9 @@
-@echo off
-SET st2Path=C:\Program Files\Sublime Text 3\sublime_text.exe
 
+@echo off
+SET pathSubl=C:\Program Files\Sublime Text 3\
+SET st2Path=%pathSubl%sublime_text.exe
+
+SETX PATH "%path%;C:\Program Files\Sublime Text 3\"
 rem add it for all file types
 @reg add "HKEY_CLASSES_ROOT\*\shell\Open with Sublime Text"         /t REG_SZ /v "" /d "Open with Sublime Text"   /f
 @reg add "HKEY_CLASSES_ROOT\*\shell\Open with Sublime Text"         /t REG_EXPAND_SZ /v "Icon" /d "%st2Path%,0" /f
