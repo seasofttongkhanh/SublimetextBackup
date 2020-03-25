94cac90daf88c0f4476b826c6945ad7d3543ff4c
khanh github <hkngokhong@gmail.com>
2020-03-25 08:30:11 +0700

    Wed Mar 25 08:30:11 SEAST 2020

--- LSP-html/html-languageserver.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\vscode-html-languageserver-bin\htmlServerMain.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\vscode-html-languageserver-bin\htmlServerMain.js" %*
-)
--- LSP-json/json-languageserver.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\vscode-json-languageserver-bin\jsonServerMain.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\vscode-json-languageserver-bin\jsonServerMain.js" %*
-)
